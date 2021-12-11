26ca3a43ec046fb0b737cb8201479cebd69d3519
gabitzzz <nl19102@essex.ac.uk>
2021-12-11 06:20:17 +0000

    10.12.2021 - before Imag eController

--- resources/js/Pages/Posts/Post.vue
+++ resources/js/Pages/Posts/Post.vue
@@ -1,31 +1,33 @@
 <template>
     <div class="mx-auto sm:mx-16 md:mx-24 my-4 lg:mx-72 xl:mx-96">
         <div class="max-2 bg-white overflow-hidden shadow-sm rounded-xl shadow-md">
-            <div class="flex">
-                <div class="flex justify-items-start mx-4 mt-4 mb-2 ">
-<!--                    <img :src="avatar" class="rounded-full avatar w-12 h-12" alt="avatar">-->
+<!--            <div class="flex">-->
+<!--                <div class="flex justify-items-start mx-4 mt-4 mb-2 ">-->
+<!--&lt;!&ndash;                    <img :src="avatar" class="rounded-full avatar w-12 h-12" alt="avatar">&ndash;&gt;-->
 
-                                    <img
-                                        :src="showImage() + post.user.avatar"
-                                        class="avatar rounded-full avatar w-12 h-12"
-                                        alt="avatar"
-                                    />
+<!--                                    <img-->
+<!--                                        :src="showImage() + post.user.avatar"-->
+<!--                                        class="avatar rounded-full avatar w-12 h-12"-->
+<!--                                        alt="avatar"-->
+<!--                                    />-->
 
-                    <p class="mt-2 ml-2">
-                        {{ post.user.username }}
-                    </p>
-                </div>
-                <inertia-link preserve-scroll
-                              v-if="post.user.id === $page.props.auth.user.id"
-                              class="   "
-                              as="button"
-                              :href="route('posts.edit', post.id)"
-                >
-                    ...
-                </inertia-link>
+<!--                    <p class="mt-2 ml-2">-->
+<!--                        {{ post.user.username }}-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--                <inertia-link preserve-scroll-->
+<!--                              v-if="post.user.id === $page.props.auth.user.id"-->
+<!--                              class="   "-->
+<!--                              as="button"-->
+<!--                              :href="route('posts.edit', post.id)"-->
+<!--                >-->
+<!--                    ...-->
+<!--                </inertia-link>-->
 
 
-            </div>
+<!--            </div>-->
+
+            {{post}}
 
 
             <div class="px-4 pb-4 border-b border-gray-200">
