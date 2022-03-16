9a71eb49d7ce0dff40235aa74af137ceee0d159b
gabitzzz <nl19102@essex.ac.uk>
2022-03-16 12:03:32 +0000

    15.03 - BEFORE MESSAGES

--- resources/views/vendor/Chatify/layouts/favorite.blade.php
+++ resources/views/vendor/Chatify/layouts/favorite.blade.php
@@ -1,6 +1,23 @@
 <div class="favorite-list-item">
-    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m" 
-        style="background-image: url('{{ asset('/storage/'.config('chatify.user_avatar.folder').'/'.$user->avatar) }}');">
-    </div>
+    {{--    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m"--}}
+    {{--         style="background-image: url('{{ asset('/storage/'.config('chatify.user_avatar.folder').'/'.$user->avatar) }}');">--}}
+
+
+    @if($user->avatar == null)
+        <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m">
+
+            <img src="{{asset("img/Tab/user.png")}}"
+                 {{--                        src="defaultProfile"--}}
+                 class="avatar  avatar w-10 h-10 ml-2 p-2"
+                 alt="default"
+            />
+        </div>
+    @else
+        <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m">
+
+            <img src="{{asset("storage/". $user->avatar)}}" alt="pic">
+        </div>
+    @endif
+    {{--</div>--}}
     <p>{{ strlen($user->name) > 5 ? substr($user->name,0,6).'..' : $user->name }}</p>
 </div>
