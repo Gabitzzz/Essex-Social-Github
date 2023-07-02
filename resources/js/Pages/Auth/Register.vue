1bd66990a29a292ad16de6f192e29d7094225847
Gabi Lițcan <64235031+Gabitzzz@users.noreply.github.com>
2023-07-02 06:47:17 +0300

    Cristi

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,154 +1,156 @@
 <template>
-    <Head title="Register"/>
+   <div>
+       <Head title="Register"/>
 
-    <div class="">
+       <div class="">
 
-        <h1 class="text-4xl px-2 text-center mb-4" style="font-family: 'Poppins', sans-serif;">
-            <!--                        DASHBOARD-->
-            Register
-        </h1>
+           <h1 class="text-4xl px-2 text-center mb-4" style="font-family: 'Poppins', sans-serif;">
+               <!--                        DASHBOARD-->
+               Register
+           </h1>
 
-        <BreezeValidationErrors class="mb-4"/>
-        <form @submit.prevent="submit">
-            <div>
-                <BreezeLabel for="name" value="Name" class="flex items-center justify-center"/>
-                <BreezeInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
-                             autocomplete="name"/>
-            </div>
+           <BreezeValidationErrors class="mb-4"/>
+           <form @submit.prevent="submit">
+               <div>
+                   <BreezeLabel for="name" value="Name" class="flex items-center justify-center"/>
+                   <BreezeInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
+                                autocomplete="name"/>
+               </div>
 
-            <div class="mt-8">
-                <BreezeLabel for="username" value="Username" class="flex items-center justify-center"/>
-                <BreezeInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" required
-                             autofocus autocomplete="username"/>
-            </div>
+               <div class="mt-8">
+                   <BreezeLabel for="username" value="Username" class="flex items-center justify-center"/>
+                   <BreezeInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" required
+                                autofocus autocomplete="username"/>
+               </div>
 
-            <div class="mt-8">
-                <div class="flex justify-center items-center">
-                    <BreezeLabel for="email" value="Poli Email" class="flex items-center justify-center"/>
+               <div class="mt-8">
+                   <div class="flex justify-center items-center">
+                       <BreezeLabel for="email" value="Poli Email" class="flex items-center justify-center"/>
 
 
-                </div>
-                <div class="flex justify-between items-center">
-                    <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
-                                 autocomplete="email"
-                                 placeholder="john.doe@stud.etti.upb.ro"
-                    />
-                    <div class="absolute right-0 tooltip text-sm justify-end px-2 bg-white rounded-full">?
-                        <span class="tooltiptext text-sm">
+                   </div>
+                   <div class="flex justify-between items-center">
+                       <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
+                                    autocomplete="email"
+                                    placeholder="john.doe@stud.etti.upb.ro"
+                       />
+                       <div class="absolute right-0 tooltip text-sm justify-end px-2 bg-white rounded-full">?
+                           <span class="tooltiptext text-sm">
                             Are you a Poli alumni? <br>
                             Use the Poli Email Address you had during your time here.
                         </span>
-                    </div>
-                </div>
-            </div>
+                       </div>
+                   </div>
+               </div>
 
-            <div class="mt-8">
-                <BreezeLabel for="password" value="Password" class="flex items-center justify-center"/>
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                             autocomplete="new-password"/>
-            </div>
+               <div class="mt-8">
+                   <BreezeLabel for="password" value="Password" class="flex items-center justify-center"/>
+                   <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                                autocomplete="new-password"/>
+               </div>
 
-            <div class="mt-8">
-                <BreezeLabel for="password_confirmation" value="Confirm Password"
-                             class="flex items-center justify-center"/>
-                <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full"
-                             v-model="form.password_confirmation" required autocomplete="new-password"/>
-            </div>
+               <div class="mt-8">
+                   <BreezeLabel for="password_confirmation" value="Confirm Password"
+                                class="flex items-center justify-center"/>
+                   <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full"
+                                v-model="form.password_confirmation" required autocomplete="new-password"/>
+               </div>
 
-            <div class="flex items-center justify-center my-8">
-                <a v-on:click="isHidden = !isHidden" class="flex justify-center bg-black rounded-full text-white w-full py-1">
-                    {{isHidden ? 'NEXT' : 'BACK'}}
-                </a>
-
-
-            </div>
+               <div class="flex items-center justify-center my-8">
+                   <a v-on:click="isHidden = !isHidden" class="flex justify-center bg-black rounded-full text-white w-full py-1">
+                       {{isHidden ? 'NEXT' : 'BACK'}}
+                   </a>
 
 
-            <div v-if="!isHidden">
-                <div>
-                    <BreezeLabel for="dob" value="Date of Birth" class="mt-6 mb-1 flex items-center justify-center"/>
-                    <DatePicker
-                        v-model="form.dob"
-                        is-expanded
-                        is24hrusername
-                        format="YYYY-MM-DD"
-                        :masks="{ L: 'YYYY-MM-DD' }"
-                        mode="date"
-                        :max-date='new Date()'
-                    >
-                    </DatePicker>
-                </div>
-
-                <div class="my-8">
-                    <BreezeLabel for="type" value="Are you.." class="flex mb-1 items-center justify-center"/>
-                    <vSelect id="type" type="text" v-model="form.type" class="border-none"
-                             :options="['Undergraduate', 'Postgraduate', 'Staff', 'Alumni', 'Other']"></vSelect>
-                </div>
-
-                <div v-if="form.type === 'Undergraduate'">
-                    <div class="my-8">
-                        <BreezeLabel for="study_year" value="Study Year" class="flex mb-1 items-center justify-center"/>
-                        <vSelect id="study_year" type="text" v-model="form.study_year" class="border-none"
-                                 :options="['1st Year', '2nd Year', '3rd Year', '4th Year', ]"></vSelect>
-                    </div>
-                </div>
-
-                <div v-if="form.type === 'Postgraduate'">
-                    <div class="my-8">
-                        <BreezeLabel for="study_year" value="Study Year" class="flex mb-1 items-center justify-center"/>
-                        <vSelect id="study_year" type="text" v-model="form.study_year" class="border-none"
-                                 :options="['1st Year Masters', '2nd Year Masters', 'PHD']"></vSelect>
-                    </div>
-                </div>
-
-                <div class="my-8" v-if="form.type === 'Postgraduate' || form.type === 'Undergraduate'">
-                    <BreezeLabel for="degree" value="You are Studying" class="flex mb-1 items-center justify-center"/>
-                    <select class="form-control w-full" name="type" v-model="form.degree">
-                        <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
-                            {{ degree.title }}
-                        </option>
-                    </select>
+               </div>
 
 
-                </div>
+               <div v-if="!isHidden">
+                   <div>
+                       <BreezeLabel for="dob" value="Date of Birth" class="mt-6 mb-1 flex items-center justify-center"/>
+                       <DatePicker
+                           v-model="form.dob"
+                           is-expanded
+                           is24hrusername
+                           format="YYYY-MM-DD"
+                           :masks="{ L: 'YYYY-MM-DD' }"
+                           mode="date"
+                           :max-date='new Date()'
+                       >
+                       </DatePicker>
+                   </div>
 
-                <div class="my-8" v-if="form.type === 'Alumni'">
-                    <BreezeLabel for="degree" value="You have Studied" class="flex mb-1 items-center justify-center"/>
-                    <select class="form-control w-full" name="type" v-model="form.degree">
-                        <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
-                            {{ degree.title }}
-                        </option>
-                    </select>
-                </div>
+                   <div class="my-8">
+                       <BreezeLabel for="type" value="Are you.." class="flex mb-1 items-center justify-center"/>
+                       <vSelect id="type" type="text" v-model="form.type" class="border-none"
+                                :options="['Undergraduate', 'Postgraduate', 'Staff', 'Alumni', 'Other']"></vSelect>
+                   </div>
 
-                <div v-if="form.type === 'Staff'">
-                    <div class="my-8">
-                        <BreezeLabel for="degree" value="Faculty" class="flex mb-1 items-center justify-center"/>
-                        <select class="form-control w-full" name="type" v-model="form.degree">
-                            <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
-                                {{ degree.title }}
-                            </option>
-                        </select>
-                    </div>
-                </div>
+                   <div v-if="form.type === 'Undergraduate'">
+                       <div class="my-8">
+                           <BreezeLabel for="study_year" value="Study Year" class="flex mb-1 items-center justify-center"/>
+                           <vSelect id="study_year" type="text" v-model="form.study_year" class="border-none"
+                                    :options="['1st Year', '2nd Year', '3rd Year', '4th Year', ]"></vSelect>
+                       </div>
+                   </div>
+
+                   <div v-if="form.type === 'Postgraduate'">
+                       <div class="my-8">
+                           <BreezeLabel for="study_year" value="Study Year" class="flex mb-1 items-center justify-center"/>
+                           <vSelect id="study_year" type="text" v-model="form.study_year" class="border-none"
+                                    :options="['1st Year Masters', '2nd Year Masters', 'PHD']"></vSelect>
+                       </div>
+                   </div>
+
+                   <div class="my-8" v-if="form.type === 'Postgraduate' || form.type === 'Undergraduate'">
+                       <BreezeLabel for="degree" value="You are Studying" class="flex mb-1 items-center justify-center"/>
+                       <select class="form-control w-full" name="type" v-model="form.degree">
+                           <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
+                               {{ degree.title }}
+                           </option>
+                       </select>
 
 
-                <BreezeButton class="flex justify-center bg-black rounded-full text-white w-full py-1 text-md" :disabled="form.processing">
-                    Register
-                </BreezeButton>
-            </div>
+                   </div>
+
+                   <div class="my-8" v-if="form.type === 'Alumni'">
+                       <BreezeLabel for="degree" value="You have Studied" class="flex mb-1 items-center justify-center"/>
+                       <select class="form-control w-full" name="type" v-model="form.degree">
+                           <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
+                               {{ degree.title }}
+                           </option>
+                       </select>
+                   </div>
+
+                   <div v-if="form.type === 'Staff'">
+                       <div class="my-8">
+                           <BreezeLabel for="degree" value="Faculty" class="flex mb-1 items-center justify-center"/>
+                           <select class="form-control w-full" name="type" v-model="form.degree">
+                               <option :value="degree.id" v-for="degree in degrees" :key="degree.id">
+                                   {{ degree.title }}
+                               </option>
+                           </select>
+                       </div>
+                   </div>
 
 
-            <!--            <h1 v-if="!isHidden">Hide me on click event!</h1>-->
+                   <BreezeButton class="flex justify-center bg-black rounded-full text-white w-full py-1 text-md" :disabled="form.processing">
+                       Register
+                   </BreezeButton>
+               </div>
 
-            <div class="flex items-center justify-center mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
-            </div>
-        </form>
 
-    </div>
+               <!--            <h1 v-if="!isHidden">Hide me on click event!</h1>-->
+
+               <div class="flex items-center justify-center mt-4">
+                   <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                       Already registered?
+                   </Link>
+               </div>
+           </form>
+
+       </div>
+   </div>
 </template>
 
 <script>
